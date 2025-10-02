0a8a47fa87257a54bb638dca22376db21003c94a
ChainsQueen.eth <146336747+ChainsQueenEth@users.noreply.github.com>
2025-10-02 20:30:06 +0100

    chore: migrate project from JavaScript to TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "./App.css";
 
 function App() {
-
   return (
     <>
       <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white text-4xl font-bold">
